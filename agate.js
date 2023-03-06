0c02828eabeeecfad9b0d6fa2e40a65afd1704cb
mia.j.lawrence <mchaulawrence@gmail.com>
2023-03-05 22:29:20 -0800

    standardized tiles js

--- agate.js
+++ agate.js
@@ -2,7 +2,7 @@
 
     let myProfile = [
     
-        {
+        /*{
         "Image":"princeton-campus.jpeg",
         "College":"college-stats.html", //create html file later
         "Name":"Princeton University",
@@ -12,7 +12,7 @@
         },
 
    
-        /*{
+        {
         "Image":" ",
         "College":" ",
         "Name":" ",
@@ -42,5 +42,66 @@
         document.getElementById('profile-post').innerHTML = profileHtml;  
     }    
     renderProfile(myProfile);
+    
+    
+    var myAdventure = {
         
+        theBeginning: {
+            "statement": "Welcome to my game. Lets Play!",
+            "background_image": "..some..image",    
+            "choices": [
+                { 
+                    "label": "Click me",
+                    "moveto": "StartPlaying"
+                }
+            ]
+        },
+
+        StartPlaying: { 
+            "statement": "You are broke and must get a job. Do you become a fisherman or get a job in a canning factory?",
+            "choices": [
+                { 
+                    "label": "Become a fisherman",
+                    "moveto": "becomeFisherman",
+                },
+                { 
+                    "label": "Get a job in a canning factory",
+                    "moveto": "becomeCanMan"
+                },
+                
+            ]
+        },
+
+        becomeFisherman: { 
+            "statement": "You have become a fisherman. However, it does not take you long to realize that there is only money in big scale fishing. However, you are already beginning to see the toll it is taking on the Monterrey bay . . .",
+            "choices": [
+                { 
+                    "label": "Your family must eat, they are more important than sardines",
+                    "moveto": "bigScaleFishing",
+                },
+                {
+                    "label": "No, you will stick to your morals",
+                    "moveto": "smallScaleFishing",
+                }
+                
+            ]
+        },
+
+        becomeCanMan: { 
+            "statement": "While undesirable, the family must eat. The hours and conditions are terrible, do you dare complain?",
+            "choices": [
+                { 
+                    "label": "Complain to boss",
+                    "moveto": "complainToBoss",
+                },
+                {
+                    "label": "No, you cannot lose your job",
+                    "moveto": "ignoreProblems",
+                },
+                
+            ]
+        },
+    
+    /*end*/ 
+    };
 }());
